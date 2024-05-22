1451e017b9268a0701d269ef975f30635c8f2f60
unknown <mynamerahulkumar13@gmail.com>
2024-05-22 19:12:15 +0530

    updated spring boot

---
+++ copilot-ecommerce-java-springboot/src/main/java/com/ecommerce/copilotecommercejavaspringboot/CopilotEcommerceJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.copilotecommercejavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotEcommerceJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotEcommerceJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ copilot-ecommerce-java-springboot/src/test/java/com/ecommerce/copilotecommercejavaspringboot/CopilotEcommerceJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.copilotecommercejavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotEcommerceJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
